594bc96971018dcbc4b0b0a202da2d835c04a063
LinqToException <47725963+LinqToException@users.noreply.github.com>
2020-02-23 01:32:29 +0100

    Fixed animation curves clamping
    Apparently WrapMode does not align up as well as I had hoped

--- YAMLSON/AnimationCurveConverter.cs
+++ YAMLSON/AnimationCurveConverter.cs
@@ -33,10 +33,31 @@ namespace Splawft.YAMLSON
                     inWeight = key.inWeight,
                     outWeight = key.outWeight
                 }).ToList(),
-                m_PreInfinity = (int)curve.preWrapMode,
-                m_PostInfinity = (int)curve.postWrapMode,
+                m_PreInfinity = ConvertWrapMode(curve.preWrapMode),
+                m_PostInfinity = ConvertWrapMode(curve.postWrapMode),
                 m_RotationOrder = 4  // https://xkcd.com/221/ - for reals, are other values possible?
             });
+        }
+
+        // Apparently, Unity's internal serialization and WrapMode do not add up
+        // (I blame legacy stuff)
+        // For AnimationCurves in recent-ish Unity, there's only 3 modes:
+        // PingPong (0), Loop (1) and Clamp (2)
+        // WrapMode knows a "few" more
+        private static int ConvertWrapMode(WrapMode mode)
+        {
+            switch (mode)
+            {
+                case WrapMode.Clamp:
+                case WrapMode.ClampForever:
+                case WrapMode.Default:
+                default:
+                    return 2;
+                case WrapMode.Loop:
+                    return 1;
+                case WrapMode.PingPong:
+                    return 0;
+            }
         }
     }
 }
